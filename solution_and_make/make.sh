4318c990cfe13d414a2b364c1d3d98178c22c48e
ocean1 <ocean.kuzuri@gmail.com>
2015-04-16 11:57:01 +0200

    updated make script

--- solution_and_make/make.sh
+++ solution_and_make/make.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # cleanup
 rm keygen.py.gz
 rm Ub7XL8T.png
@@ -5,8 +6,10 @@ rm keygen.xored.gz
 rm embed.com
 
 # build
-cat keygen.py | gzip -n -o keygen.py.gz
+cat keygen.py | gzip -n > keygen.py.gz
 python create_xored_gz.py
 nasm embeddedcom.s -o embed.com
 python gen_image.py
 cp Ub7XL8T.png ../
+
+echo "\nDONE!\n"
