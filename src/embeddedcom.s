05880deadccb079c150641a77d921bcb10881e99
ocean1 <ocean.kuzuri@gmail.com>
2015-06-25 14:28:35 +0200

    moved solution to src

--- src/embeddedcom.s
+++ src/embeddedcom.s
@@ -1,22 +1,45 @@
 org 100h
 section .text
 
-jmp loop
     nop
     nop
     nop
     nop
-loop:
-    jmp loop
+    jmp copy_prot
 
-mov ah,03Ch        ; the open/create-a-file function
+int13call:
+    pushf          ;pusha flags                   ; good old copy-protection baby
+    mov  bx,004ch  ;address of INT_13 vector      ; run me on an old floppy drive :)
+    push word[bx+02]   ;push CS of INT_13 routine
+    push word[bx]      ;push IP of INT_13 routine
+    iret           ;pop IP,CS and flags
+
+copy_prot:
+    mov  ah,04     ;read operation
+    mov  al,01     ;1 sector to read
+    mov  ch,29h    ;track 29h
+    mov  cl,0ffh    ;sector ffh
+    mov  dx,0000   ;side 0, drive A
+    mov  bx, cs
+    ;xor  bx,bx     ;move 0...
+    mov  ds,bx     ;...to DS register
+    mov  es,bx     ;...to BX register
+    mov  bx, writehere
+    pushf          ;pusha flags
+    push cs        ;pusha CX
+    call int13call ;push address for next
+    or ah,ah       ;check CRC error
+
+    jnz exit       ;error reading diskette? piss off!
+
+    mov ah,03Ch        ; the open/create-a-file function
     mov cx,020h        ; file attribute - normal file
     mov dx,msg    ; address of a ZERO TERMINATED! filename string
     int 021h           ; call on Good Old Dos
 
-jc exit
+    jc exit
 
-mov si,ax  ; returns a file handle (probably 5)
+    mov si,ax  ; returns a file handle (probably 5)
 
 ; ------ here decrypt file -------
     xor bx, bx
@@ -29,13 +52,13 @@ decrypt:
     jnz decrypt
 
 
-mov ah,040h        ; the Write-to-a-file function for int 21h
+    mov ah,040h        ; the Write-to-a-file function for int 21h
     mov bx,si  ; the file handle goes in bx
     mov cx,280  ; number of bytes to write
     mov dx,gzipf     ; address to write from (the text we input)
     int 021h           ; call on Good Old Dos
 
-mov ah,03Eh        ; the close-the-file function
+    mov ah,03Eh        ; the close-the-file function
     mov bx,si  ; the file handle
     int 021h           ; call on Good Old Dos
 
@@ -52,3 +75,4 @@ section .data
     gzipf   incbin "keygen.xored.gz"
 
 section .bss
+    writehere resb 1
