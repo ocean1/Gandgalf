569149c2947c97bfae89b28dce9a32e2fcc6b63e
ocean1 <ocean.kuzuri@gmail.com>
2015-06-26 08:57:51 +0200

    added ignore, jump magic of compressed file

--- src/embeddedcom/make.sh
+++ src/embeddedcom/make.sh
@@ -1,5 +1,5 @@
 rm *.com
 nasm embeddedcom.s -o main.com
 lz4c -y -l main.com main.lz4 2> /dev/null
-nasm unpack.s -o unpack.com
-cat unpack.com main.lz4 > embed.com
+nasm unpack.s -o embed.com
+tail -c +3 main.lz4 >> embed.com
