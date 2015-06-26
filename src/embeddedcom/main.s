2bf0d71b48ad114e9016070ed8b11d8a5cbf12ab
ocean1 <ocean.kuzuri@gmail.com>
2015-06-26 17:25:22 +0200

    some more stuff for encryption

--- src/embeddedcom/main.s
+++ src/embeddedcom/main.s
@@ -18,37 +18,65 @@ intcall:
     push word[ds:si+02]   ;push CS of INT routine
     push word[ds:si]      ;push IP of INT routine
     mov ds, bp
+iretinstr:
     iret           ;pop IP,CS and flags
+
+intgetaddr:
+    ; pass via SI the interrupt number
+    xor ax, ax
+    mov  ds, ax     ;...to DS register
+    shl si, 1
+    shl si, 1
+    push word[ds:si+02]   ;push CS of INT routine
+    push word[ds:si]      ;push IP of INT routine
+    pop si
+    pop ds
+    ret
 
 copy_prot:
     mov  ah,02     ;read operation
-    mov  al,01     ;1 sector to read
-    xor cx, cx     ;read the first sector
+    mov  al,1     ;1 sector to read
+    xor cx, cx
+    inc cl          ;read the first sector
     xor dx, dx   ;side 0, drive A
     mov  bx, writehere
     pushf          ;pusha flags
     push cs        ;pusha CS
     mov si, 13h
     call intcall ;push address for next
-    ;jc exit
+    ;jc exit ; at worst we are reading uninitialized mem (:
 
 
 end_copy_prot:
 
     mov ah,03Ch        ; the open/create-a-file function
     mov cx,020h        ; file attribute - normal file
-    mov dx,msg    ; address of a ZERO TERMINATED! filename string
+    mov dx,msg         ; address of a ZERO TERMINATED! filename string
     int 021h           ; call on Good Old Dos
 
     jc exit
 
-    mov si,ax  ; returns a file handle (probably 5)
+    push ax             ; store file handle
 
 ; ------ here decrypt file -------
     xor bx, bx
+    push ds
+
+    call intgetaddr
+    ; if no debugger is found int3 handler should be IRET = CF
+    mov cl, [ds:si]
+
+    pop ds
+    jmp decrypt
+
+    gzipf   incbin "../keygen/keygen.xored.gz"
+    gzipend:
+
 decrypt:
     mov al, [gzipf+bx]
+    sub al, cl
     xor al, [writehere]
+    xor cl, [gzipf+bx]
     mov [gzipf+bx], al
     inc bx
     cmp bx, (gzipend - gzipf)
@@ -56,7 +84,7 @@ decrypt:
 
 
     mov ah,040h        ; the Write-to-a-file function for int 21h
-    mov bx,si  ; the file handle goes in bx
+    pop bx  ; the file handle goes in bx
     mov cx,280  ; number of bytes to write
     mov dx,gzipf     ; address to write from (the text we input)
     int 021h           ; call on Good Old Dos
@@ -75,8 +103,6 @@ section .data
     ; let`s fool binwalk users :)
     ; fool    db `\x1f\x8b\x08\x00\xb5\xcb.U\x00\x03+\xc8,\xa9L\xcb\xcf\xcf\xc9\xcb/IL\xcbIL\xe7\x02\x00~\x80D\xdf\x11\x00\x00\x00`
 ;--------------------------------------------------
-    gzipf   incbin "../keygen/keygen.xored.gz"
-    gzipend:
     ;global _something_size
     ;_something_size dd _something_end - _something
 
--- src/keygen/create_xored_gz.py
+++ src/keygen/create_xored_gz.py
@@ -3,10 +3,15 @@ import sys
 out = ""
 with open(sys.argv[1], 'r') as kg:
 
-    key = 0xeb
+    key = 0xEB
     keyfile = kg.read()
+    addition = 0xFE
     for i in keyfile:
-        out += chr(ord(i) ^ key)
+        out += chr((
+                (ord(i) ^ key) +
+                addition )&0xFF
+            )
+        addition ^= ord(out[-1:])
     # print "\\x".join("{:02x}".format(ord(c) ^ key) for c in keyfile)
     # print out.__repr__()
 
